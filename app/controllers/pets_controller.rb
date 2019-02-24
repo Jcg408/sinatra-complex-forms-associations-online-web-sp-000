c89b3be34b780ea98b3b9e2ab647557cb4c28b2a
Jessica Gladstone <jcg321@comcast.net>
2019-02-24 19:40:06 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,7 +9,7 @@ class PetsController < ApplicationController
   end
   
   post '/pets' do 
-  @pet = Pet.create(params[:owner])
+  @pet = Owner.create(params[:owner])
     if !params[:owner][:name].empty?
       @pet.owners << Owner.create(name: params[:owner][:name])
      

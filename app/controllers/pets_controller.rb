aaa6e054e0c597853392227d5e1f2660017b8b8b
Jessica Gladstone <jcg321@comcast.net>
2019-02-24 19:43:25 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -18,17 +18,17 @@ class PetsController < ApplicationController
     
   end
   get '/pets/:id/edit' do
-    @pet= Pet.find_by_id(params[:id])
+    @pet= Pet.find(params[:id])
     erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
-    @pet= Pet.find_by_id(params[:id])
+    @pet= Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do 
-    @pet= Pet.find_by_id(params[:id])
+    @pet= Pet.find(params[:id])
      @pet.update(params[:pet])
    if !params[:owner][:name].empty?
       @owner.pets << Pet.create(name: params[:owner][:name])
